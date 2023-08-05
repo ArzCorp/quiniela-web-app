091c34bf1c447cc7d8ea2c67cf144ad24b156f0c
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-08-05 12:37:23 -0600

    🔥 clean project

--- src/components/Layout.js
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children, center }) {
-  const centerStyles = center ? 'flex justify-center items-center' : ''
-
-  return (
-    <main className={`bg-gray-900 min-h-screen w-full ${centerStyles}`}>
-      {children}
-    </main>
-  )
-}
--- src/components/RegisterForm.js
+++
@@ -1,7 +0,0 @@
-export default function RegisterForm() {
-  return (
-    <form className="flex flex-col gap-4 max-w-[380px] mx-auto" onSubmit="">
-      <p>Form aqui</p>
-    </form>
-  )
-}
